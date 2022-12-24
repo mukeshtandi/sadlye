9599a173e6a2d2b937e5a85a05e2ab5921bfe2a7
MUKESH TANDI <42092980+mukeshtandi@users.noreply.github.com>
2022-12-24 18:38:04 +0530

    Create data23.js

---
+++ gg/data23.js
@@ -0,0 +1,33 @@
+function getQueryParam(e){var a=window.location.search.match(new RegExp("(\\?|&)"+e+"(\\[\\])?=([^&]*)"));return!!a&&a[3]};
+const name=decodeURIComponent(getQueryParam("n").replace(/-|@|%40|\+|%20/g," "));
+const blankname = 'Your Name';
+const marquee = [
+'<img src="https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/sas1.png" style="height:35px; width:35px;" /><br /><br />',
+'<img src="https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/sas2.png" style="height:35px; width:35px;" /><br /><br />',
+'<img src="https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/sas3.png" style="height:35px; width:35px;" /><br /><br />',
+'<img src="https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/sas4.png" style="height:35px; width:35px;" /><br /><br />',
+'<img src="https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/sas5.png" style="height:35px; width:35px;" /><br /><br />',
+'<img src="https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/sas6.png" style="height:35px; width:35px;" /><br /><br />',
+'<img src="https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/sas7.png" style="height:35px; width:35px;" /><br /><br />',
+'<img src="https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/sas8.png" style="height:35px; width:35px;" /><br /><br />',
+'',
+''
+];
+const picwtsp = 'https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/wtsp.png';
+const fbpic = 'https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/fb.png';
+const messengerpic = 'https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/messenger.png';
+const sharewpmobi = '*'+name+'* hat dir ein schönes Geschenk.🎁%0A Berühre diese blaue Linie und sieh nach.%0A👇👇👇%0A https://sadlye.com/gg/?n=';
+const sharefbmobi = 'https%3A%2F%2Fsadlye.com/gg/?n=';
+const fbID = '413498309346615';
+const wpbtnName = 'Share on Whatsapp';
+const FbLinkBlockExtraText = '';
+
+$('#wish1').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/chgrwish1.png');
+$('#main1').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/chgrmain1.png');
+$('#under1').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/santaji4.gif');
+$('#under2').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/santaji5.gif');
+
+$('#wish2').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/chgrwish2.png');
+$('#main2').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/chgrmain2.png');
+$('#under3').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/santaji6.gif');
+$('#under4').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/sadlye/gg/santaji7.gif');
